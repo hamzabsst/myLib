d0f625bccabd6d895143a9f48ef42af2a6d424c1
Hamza Boussetta <hbousset@c3r1p10.1337.ma>
2025-01-20 14:41:25 +0100

    fixed norm

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: hbousset < hbousset@student.1337.ma>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 18:12:32 by hbousset          #+#    #+#             */
-/*   Updated: 2025/01/16 18:01:30 by hbousset         ###   ########.fr       */
+/*   Updated: 2025/01/20 14:40:51 by hbousset         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,4 +30,3 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	}
 	return (dest);
 }
-
--- ft_striteri.c
+++ ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: hbousset < hbousset@student.1337.ma>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 10:29:34 by hbousset          #+#    #+#             */
-/*   Updated: 2025/01/16 18:02:27 by hbousset         ###   ########.fr       */
+/*   Updated: 2025/01/20 14:41:00 by hbousset         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,4 +25,3 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 		i++;
 	}
 }
-
